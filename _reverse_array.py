595ee61ade946c9cc0b5622a5a2f6a79c165b66d
soujanyareddyM <121964113+soujanyareddyM@users.noreply.github.com>
2023-03-07 22:14:20 +0530

    Add files via upload

---
+++ _reverse_array.py
@@ -0,0 +1,12 @@
+def reverse_array(arr):
+    left = 0
+    right = len(arr) - 1
+    while left < right:
+        arr[left], arr[right] = arr[right], arr[left]
+        left += 1
+        right -= 1
+
+# Example 
+arr = [1, 2, 3, 4, 5]
+reverse_array(arr)
+print(arr)  
